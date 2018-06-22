ad65cd7687e9ed4749c524ef4a845ee3363af992
limengke <752140235@qq.com>
2018-06-22 17:45:34 +0800

    init project

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -90,7 +90,7 @@ module.exports = {
     // for React Native Web.
     extensions: ['.web.js', '.mjs', '.js', '.json', '.web.jsx', '.jsx'],
     alias: {
-      
+
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web',
@@ -121,7 +121,7 @@ module.exports = {
             options: {
               formatter: eslintFormatter,
               eslintPath: require.resolve('eslint'),
-              
+
             },
             loader: require.resolve('eslint-loader'),
           },
@@ -149,9 +149,25 @@ module.exports = {
             include: paths.appSrc,
             loader: require.resolve('babel-loader'),
             options: {
-              
+
               compact: true,
             },
+          },
+          // stylus-loader
+          {
+              test:/\.styl/,
+              use:[
+                  'style-loader',
+                  //modules 开启css modules
+                  'css-loader?modules&localIdentName=[name]__[local]-[hash:base64:5]',
+                  {
+                      loader:'postcss-loader',
+                      options:{
+                          sourceMap:true
+                      }
+                  },
+                  'stylus-loader'
+              ]
           },
           // The notation here is somewhat confusing.
           // "postcss" loader applies autoprefixer to our CSS.

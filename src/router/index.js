70e9502a548249223aa3105a48e1f7e6a4ef44fe
limengke <752140235@qq.com>
2018-06-28 16:20:45 +0800

    第四个动画

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,8 @@ import ArticlePage from '../pages/article-page'
 import MotionDemo1 from '../pages/motion-page/demo1/motion-demo1'
 import MotionDemo2 from '../pages/motion-page/demo2/motion-demo2'
 import MotionDemo3 from '../pages/motion-page/demo3/motion-demo3'
+import MotionDemo4 from '../pages/motion-page/demo4/motion-demo4'
+import MotionDemo5 from '../pages/motion-page/demo5/motion-demo5'
 
 const MotionPage = ({match}) => (
     <Fragment>
@@ -15,11 +17,15 @@ const MotionPage = ({match}) => (
                 <Link to={`${match.url}/demo1`}>demo1</Link>
                 <Link to={`${match.url}/demo2`}>demo2</Link>
                 <Link to={`${match.url}/demo3`}>demo3</Link>
+                <Link to={`${match.url}/demo4`}>demo4</Link>
+                <Link to={`${match.url}/demo5`}>demo5</Link>
             </Fragment>
             )} />
         <Route path={`${match.url}/demo1`} component={MotionDemo1}/>
         <Route path={`${match.url}/demo2`} component={MotionDemo2}/>
         <Route path={`${match.url}/demo3`} component={MotionDemo3}/>
+        <Route path={`${match.url}/demo4`} component={MotionDemo4}/>
+        <Route path={`${match.url}/demo5`} component={MotionDemo5}/>
     </Fragment>
 )
 
